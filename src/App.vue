182ee12797c4bcc908f47f4dc088f5fb58f700a0
Moch Dhandy Friyadi <dhandy@orderonline.id>
2025-02-12 20:50:41 +0700

    init

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+<div id="app">
+  <main class="bg-slate-800">
+    <router-view></router-view>
+  </main>
+</div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,5 @@
+<template>
+<div id="home" class="min-h-screen max-w-6xl mx-auto text-white px-6 py-12 md:px-12 md:py-16 lg:py-0">
+	<h1>Portfolio</h1>
+</div>
+</template>
