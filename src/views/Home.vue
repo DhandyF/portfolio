a3c2eb8d495394d35d9a841d4c21c7aa054dff8a
Moch Dhandy Friyadi <dhandy@orderonline.id>
2025-02-26 21:13:12 +0700

    change main routes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,15 @@
 <template>
-<div id="home" class="min-h-screen max-w-6xl mx-auto text-white px-6 py-12 md:px-12 md:py-16 lg:py-0">
-	<h1>Portfolio</h1>
+<div
+	id="home"
+	class="min-h-screen max-w-6xl mx-auto text-white px-6 py-12 md:px-12 md:py-16 lg:py-0">
+	<div class="lg:flex lg:justify-between lg:gap-4">
+		<Header />
+		<div></div>
+	</div>
 </div>
 </template>
+
+<script setup>
+import Header from '@/components/Header.vue';
+
+</script>
