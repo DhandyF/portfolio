6ddecc388f95b9d863d9d2783c5ac1a31b0a0c9d
Moch Dhandy Friyadi <dhandy@orderonline.id>
2025-02-26 21:13:00 +0700

    header component

---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+	<header class="lg:max-h-screen lg:w-[45%] lg:flex lg:flex-col lg:justify-content-between lg:py-24 font-[Noto Sans]">
+		<h1 class="text-4xl sm:text-5xl font-bold text-slate-200">
+			{{ FULL_NAME }}
+		</h1>
+		<h3 class="text-xl mt-3 text-slate-200">
+			{{ JOB_ROLE }}
+		</h3>
+		<p class="mt-4 max-w-xs text-slate-400">{{ ROLE_OVERVIEW }}</p>
+	</header>
+</template>
+
+<script setup>
+import {
+	FULL_NAME,
+	JOB_ROLE,
+	ROLE_OVERVIEW
+} from '@/lib/statics';
+
+</script>
---
+++ src/lib/statics.js
@@ -0,0 +1,3 @@
+export const FULL_NAME = 'Moch Dhandy Friyadi';
+export const JOB_ROLE = 'Full Stack Engineer';
+export const ROLE_OVERVIEW = 'I build robust and dynamic website application.';
