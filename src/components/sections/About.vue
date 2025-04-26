6f193c679dc1094f4b9d146fa34b6e299ad3e5a4
Moch Dhandy Friyadi <dhandy@orderonline.id>
2025-04-26 21:12:45 +0700

    about section component

---
+++ src/components/sections/About.vue
@@ -0,0 +1,14 @@
+<template>
+<div id="about" class="text-slate-500">
+	<div class="mb-4 py-4 lg:sr-only">
+		<h2 class="text-sm text-bold text-slate-200">ABOUT</h2>
+	</div>
+	<p>
+		{{ ABOUT }}
+	</p>
+</div>
+</template>
+
+<script setup>
+import { ABOUT } from '@/lib/statics';
+</script>
--- src/lib/statics.js
+++ src/lib/statics.js
@@ -1,3 +1,4 @@
 export const FULL_NAME = 'Moch Dhandy Friyadi';
 export const JOB_ROLE = 'Full Stack Engineer';
 export const ROLE_OVERVIEW = 'I build robust and dynamic website application.';
+export const ABOUT = 'Passionate Fullstack Developer with over 2 years of experience building end-to-end web applications, and over 2 years of dedicated Frontend development expertise. Proficient in PHP (Laravel), Vue.js, RESTful API integration, creating responsive, and user-friendly interfaces. Adept at working in Agile environments, collaborating with cross-functional teams, and delivering clean, maintainable code. Committed to continuous learning and adopting new technologies.';
