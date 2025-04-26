a0bbaa29379e73abf0ca16f74975f473f6e0e203
Moch Dhandy Friyadi <dhandy@orderonline.id>
2025-04-26 20:25:00 +0700

    update navbar component

--- src/components/navbar/Index.vue
+++ src/components/navbar/Index.vue
@@ -1,5 +1,69 @@
 <template>
-<nav class="navbar">
-
+<nav class="navbar hidden lg:block">
+	<ul class="mt-16 w-max">
+		<li
+			v-for="menu in menus"
+			:key="menu.key"
+			class="flex items-center py-3 cursor-pointer text-slate-500"
+			@mouseenter="onFocus(menu)"
+			@mouseleave="onLeave()"
+			@click="onClick(menu)"
+		>
+			<span
+				class="mr-4 h-px w-8 bg-slate-600 transition-all motion-reduce:transition-none"
+				:class="[
+					{ 'w-16 bg-slate-200': isHoveredMenu(menu) || isActiveMenu(menu) }
+				]"
+			/>
+			<span
+				class="nav-text text-xs font-bold uppercase tracking-widest"
+				:class="[{ 'text-slate-200': isHoveredMenu(menu) || isActiveMenu(menu) }]"
+			>
+				{{ menu.title }}
+			</span>
+		</li>
+	</ul>
 </nav>
 </template>
+
+<script setup>
+import { ref } from 'vue'
+
+const menus = [
+	{
+		key: 'about',
+		title: 'About',
+	},
+	{
+		key: 'experience',
+		title: 'Experience',
+	},
+	{
+		key: 'project',
+		title: 'Project',
+	},
+];
+
+const focusedMenu = ref(null)
+const activeMenu = ref(null)
+
+const onClick = (menu) => {
+	activeMenu.value = menu
+}
+
+const onFocus = (menu) => {
+	focusedMenu.value = menu
+}
+
+const onLeave = () => {
+	focusedMenu.value = null
+}
+
+const isHoveredMenu = (menu) => {
+	return menu.key === focusedMenu?.value?.key
+}
+
+const isActiveMenu = (menu) => {
+	return menu.key === activeMenu?.value?.key
+}
+</script>
