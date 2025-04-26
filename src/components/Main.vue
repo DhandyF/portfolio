ff4d8d020f7c6bf4a1ffdcc9d33906c7c2d02c5c
Moch Dhandy Friyadi <dhandy@orderonline.id>
2025-04-26 21:12:55 +0700

    add about section

---
+++ src/components/Main.vue
@@ -0,0 +1,10 @@
+<template>
+	<main class="lg:max-h-screen lg:w-[50%] lg:flex lg:flex-col lg:justify-content-between lg:py-24 pt-24 font-[Noto Sans]">
+		<about />
+	</main>
+</template>
+
+<script setup>
+import About from '@/components/sections/About.vue'
+
+</script>
