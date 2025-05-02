15b0155f7a066b7ac1cf6f5ee59017cc91514968
Moch Dhandy Friyadi <dhandy@orderonline.id>
2025-05-02 20:28:55 +0700

    add main section

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,16 @@
 <template>
 <div
 	id="home"
-	class="min-h-screen max-w-6xl mx-auto text-white px-6 py-12 md:px-12 md:py-16 lg:py-0">
+	class="min-h-screen max-w-7xl mx-auto text-white px-6 py-12 md:px-12 md:py-16 lg:py-0">
 	<div class="lg:flex lg:justify-between lg:gap-4">
 		<Header />
-		<div></div>
+		<Main />
 	</div>
 </div>
 </template>
 
 <script setup>
 import Header from '@/components/Header.vue';
+import Main from '@/components/Main.vue';
 
 </script>
