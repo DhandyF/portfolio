ebd7106d4c3ad19d47de2aa2db548c2e6cca5d97
Moch Dhandy Friyadi <dhandy@orderonline.id>
2025-05-03 14:05:57 +0700

    change about margin

--- src/components/sections/About.vue
+++ src/components/sections/About.vue
@@ -1,5 +1,5 @@
 <template>
-<div id="about" class="text-slate-500">
+<div id="about" class="mb-16 md:mb-24 lg:mb-36 text-slate-500">
 	<div class="mb-4 py-4 lg:sr-only">
 		<h2 class="text-sm text-bold text-slate-200">ABOUT</h2>
 	</div>
