5cb3378616ac7d7603a534fef71c292a55707a23
Moch Dhandy Friyadi <dhandy@orderonline.id>
2025-05-03 14:48:11 +0700

    change indentation to tabs

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,9 @@ import { RouterView } from 'vue-router'
 
 <template>
 <div id="app">
-  <main class="bg-slate-900">
-    <router-view></router-view>
-  </main>
+	<main class="bg-slate-900">
+		<router-view></router-view>
+	</main>
 </div>
 </template>
 
