1315b1b7cb10f9166f62c5b0fad5e39291791f7a
Moch Dhandy Friyadi <dhandy@orderonline.id>
2025-05-13 23:30:37 +0700

    add experiece url

--- src/lib/statics.js
+++ src/lib/statics.js
@@ -14,6 +14,7 @@ export const EXPERIENCES = [
 		period: 'Jun 2023 - Present',
 		position: 'Full Stack Developer',
 		company: 'PT Ordivo Teknologi Indonesia',
+		url: 'https://orderonline.id/',
 		job_desc: 'Develop and maintain high uptime website application. Implement a responsive user interface for a web and mobile device, ensuring a seamless user experience across different devices. Collaborated with product owner, backend developer, and designer to defined technical specifications, proper design, and ensured alignment with business objectives.',
 		tech_stack: [
 			'JavaScript',
@@ -36,6 +37,7 @@ export const EXPERIENCES = [
 		period: 'Mar - Oct 2023',
 		position: 'Frontend Developer',
 		company: 'PT Sapta Muda Berkarya',
+		url: 'https://bpdlh.kemenkeu.go.id/',
 		job_desc: 'As Freelancer, developed and maintained internal web application for Badan Pengelola Dana Lingkungan Hidup. Implemented and enhanced user-friendly interfaces, improving both user experience and business workflow efficiency.',
 		tech_stack: [
 			'JavaScript',
@@ -52,7 +54,8 @@ export const EXPERIENCES = [
 		period: 'Feb 2021 - Jan 2023',
 		position: 'Frontend Developer',
 		company: 'PT Lawencon International',
-		job_desc: 'Developed an internal web application, migrating key features from a legacy desktop application to a modern, browser-based platform, improving accessibility and efficiency for bank employees. Initiated the adoption of Vue 3 and implemented a Micro UI (Micro Frontend) architecture.',
+		url: 'https://bri.co.id/',
+		job_desc: 'Developed an internal web application for PT Bank Rakyat Indonesia, migrating key features from a legacy desktop application to a modern, browser-based platform, improving accessibility and efficiency for bank employees. Initiated the adoption of Vue 3 and implemented a Micro UI (Micro Frontend) architecture.',
 		tech_stack: [
 			'JavaScript',
 			'Vue',
@@ -67,6 +70,7 @@ export const EXPERIENCES = [
 		period: 'Nov 2019 - May 2020',
 		position: 'Frontend Developer',
 		company: 'PT. Dtech Solusi Bisnis',
+		url: 'https://cgo.co.id/',
 		job_desc: 'As Freelancer, developed and maintained an online reservation web application. Implemented a fully responsive user interface optimized for both web and mobile devices.',
 		tech_stack: [
 			'JavaScript',
@@ -82,6 +86,7 @@ export const EXPERIENCES = [
 		period: 'Jul - Nov 2019',
 		position: 'Full Stack Developer',
 		company: 'PT Kong Teknologi Asia',
+		url: 'https://www.kongteknologiasia.com/',
 		job_desc: 'Led a team to develop an online reservation web application. Built responsive, user-friendly interfaces for web and mobile, and developed backend services integrated via RESTful APIs for smooth, reliable performance. Managed project timelines and coordinated tasks.',
 		tech_stack: [
 			'JavaScript',
--- src/views/sections/experience/Index.vue
+++ src/views/sections/experience/Index.vue
@@ -12,30 +12,32 @@
 			:key="experience.id"
 			class="mb-12 cursor-pointer lg:p-3 rounded-lg group hover:bg-white/10"
 		>
-			<div class="grid sm:grid-cols-8 sm:gap-8 md:gap-4">
-				<div class="z-10 mb-2 mt-1 text-xs font-semibold uppercase tracking-wide text-slate-500 sm:col-span-2">
-					{{ experience.period }}
+			<a :href="experience.url" target="_blank">
+				<div class="grid sm:grid-cols-8 sm:gap-8 md:gap-4">
+					<div class="z-10 mb-2 mt-1 text-xs font-semibold uppercase tracking-wide text-slate-500 sm:col-span-2">
+						{{ experience.period }}
+					</div>
+					<div class="sm:col-span-6">
+						<h3 class="font-medium text-slate-200 mb-2 group-hover:!text-teal-300">
+							<a href="">
+								<span>{{ experience.position }} · {{ experience.company }}</span>
+							</a>
+						</h3>
+						<p class="text-slate-400 text-sm leading-[1.5] mb-2">
+							{{ experience.job_desc }}
+						</p>
+						<ul class="flex flex-wrap">
+							<li
+								v-for="(tech, index) in experience.tech_stack"
+								:key="tech + index"
+								class="items-center rounded-full text-xs text-teal-300 bg-teal-400/10 py-1 px-3 mr-1 mt-2"
+							>
+								{{ tech }}
+							</li>
+						</ul>
+					</div>
 				</div>
-				<div class="sm:col-span-6">
-					<h3 class="font-medium text-slate-200 mb-2 group-hover:!text-teal-300">
-						<a href="">
-							<span>{{ experience.position }} · {{ experience.company }}</span>
-						</a>
-					</h3>
-					<p class="text-slate-400 text-sm leading-[1.5] mb-2">
-						{{ experience.job_desc }}
-					</p>
-					<ul class="flex flex-wrap">
-						<li
-							v-for="(tech, index) in experience.tech_stack"
-							:key="tech + index"
-							class="items-center rounded-full text-xs text-teal-300 bg-teal-400/10 py-1 px-3 mr-1 mt-2"
-						>
-							{{ tech }}
-						</li>
-					</ul>
-				</div>
-			</div>
+			</a>
 		</li>
 	</ol>
 </section>
