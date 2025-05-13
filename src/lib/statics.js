a024137dc3374d881f8374988eed47b68b8b053c
Moch Dhandy Friyadi <dhandy@orderonline.id>
2025-05-13 15:59:16 +0700

    update experience section

--- src/lib/statics.js
+++ src/lib/statics.js
@@ -14,7 +14,7 @@ export const EXPERIENCES = [
 		period: 'Jun 2023 - Present',
 		position: 'Full Stack Developer',
 		company: 'PT Ordivo Teknologi Indonesia',
-		job_desc: 'Developed and launched the Top-up feature, increasing transaction efficiency and contributing to a significant boost in user engagement and revenue.',
+		job_desc: 'Develop and maintain high uptime website application. Implement a responsive user interface for a web and mobile device, ensuring a seamless user experience across different devices. Collaborated with product owner, backend developer, and designer to defined technical specifications, proper design, and ensured alignment with business objectives.',
 		tech_stack: [
 			'JavaScript',
 			'Vue',
@@ -36,7 +36,7 @@ export const EXPERIENCES = [
 		period: 'Mar - Oct 2023',
 		position: 'Frontend Developer',
 		company: 'PT Sapta Muda Berkarya',
-		job_desc: 'Developed and launched the Top-up feature, increasing transaction efficiency and contributing to a significant boost in user engagement and revenue.',
+		job_desc: 'As Freelancer, developed and maintained internal web application for Badan Pengelola Dana Lingkungan Hidup. Implemented and enhanced user-friendly interfaces, improving both user experience and business workflow efficiency.',
 		tech_stack: [
 			'JavaScript',
 			'Vue',
@@ -52,7 +52,7 @@ export const EXPERIENCES = [
 		period: 'Feb 2021 - Jan 2023',
 		position: 'Frontend Developer',
 		company: 'PT Lawencon International',
-		job_desc: 'Developed and launched the Top-up feature, increasing transaction efficiency and contributing to a significant boost in user engagement and revenue.',
+		job_desc: 'Developed an internal web application, migrating key features from a legacy desktop application to a modern, browser-based platform, improving accessibility and efficiency for bank employees. Initiated the adoption of Vue 3 and implemented a Micro UI (Micro Frontend) architecture.',
 		tech_stack: [
 			'JavaScript',
 			'Vue',
@@ -67,7 +67,7 @@ export const EXPERIENCES = [
 		period: 'Nov 2019 - May 2020',
 		position: 'Frontend Developer',
 		company: 'PT. Dtech Solusi Bisnis',
-		job_desc: 'Developed and launched the Top-up feature, increasing transaction efficiency and contributing to a significant boost in user engagement and revenue.',
+		job_desc: 'As Freelancer, developed and maintained an online reservation web application. Implemented a fully responsive user interface optimized for both web and mobile devices.',
 		tech_stack: [
 			'JavaScript',
 			'Vue',
@@ -82,7 +82,7 @@ export const EXPERIENCES = [
 		period: 'Jul - Nov 2019',
 		position: 'Full Stack Developer',
 		company: 'PT Kong Teknologi Asia',
-		job_desc: 'Developed and launched the Top-up feature, increasing transaction efficiency and contributing to a significant boost in user engagement and revenue.',
+		job_desc: 'Led a team to develop an online reservation web application. Built responsive, user-friendly interfaces for web and mobile, and developed backend services integrated via RESTful APIs for smooth, reliable performance. Managed project timelines and coordinated tasks.',
 		tech_stack: [
 			'JavaScript',
 			'Vue',
